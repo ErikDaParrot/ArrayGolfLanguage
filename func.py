691c49cf18dcfe2bf21aea6212aede8f0be9b20f
ErikDaParrot <nambaole3@gmail.com>
2025-02-05 21:04:37 -0700

    Update func.py

--- func.py
+++ func.py
@@ -13,14 +13,13 @@ group = lambda x, y: [i for i, j in zip(x, y) if j] if max(y) <= 1 else \
     for i in range(1, max(y) + 1)]
 resize = lambda x, y: np.resize(list(x), tuple(y)).tolist()
 select = lambda x, y: [np.array(x)[*([i] if type(i) is int else i)].tolist() for i in y]
-ravel = lambda x: np.ravel(x).tolist()
+ravel = lambda x: np.ravel(x).tolist() if type(x) is list else x
 parse = lambda x: [main.parseNestedBracks(i) for i in main.parseLine(x)]
-trigonometry = lambda x: [0, math.sin(x), math.cos(x), math.tan(x), math.atan(x), math.acos(x), math.asin(x)]
+trigonometry = lambda x, y: [None, math.sin(x), math.cos(x), math.tan(x), math.atan(x), math.acos(x), math.asin(x)][y] if abs(x) < 4 else None
 prefix = lambda x: [x[0] if i == 1 else x[:i] for i in range(1, len(x) + 1)]
 suffix = lambda x: [x[-1] if i == 1 else x[-i:] for i in range(1, len(x) + 1)]
-reshape = lambda x, y: (resize(ravel(x), y) if 0 not in y else \
-  reshape(x, [len(ravel(x)) // math.prod([i for i in y if i != 0]) if i == 0 else i for i in y])) \
-    if y.count(0) <= 1 else None
+reshape = lambda x, y: resize(ravel(x), [len(ravel(x)) // math.prod([i for i in y if i != 0]) \
+  if not i else i for i in y]) if y.count(0) <= 1 else None
 
     
 def partition(x, y):
